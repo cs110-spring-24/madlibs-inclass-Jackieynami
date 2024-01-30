7547e4a5c2245e9c3d020b35b80d676d96486513
Jackieynami <157658018+Jackieynami@users.noreply.github.com>
2024-01-29 18:03:01 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -1,1 +1,12 @@
 # TODO Create your inputs, variables, and add your story here!
+noun = input("Enter a noun: ")
+adjective = input("Enter an adjective: ")
+verb = input("Enter a verb: ")
+
+# Add 10 more inputs, update your story along the way
+name = input("Enter aname: ")
+noun2 = input("Enter another noun: ")
+
+print(f"{noun} was {adjective} while {verb}")
+
+
