61e2751a1ad25f7a456a3edf0f633aa7e8b5b339
Jackieynami <157658018+Jackieynami@users.noreply.github.com>
2024-02-07 12:02:04 -0800

    Update madlibs.py

--- madlibs.py
+++ madlibs.py
@@ -4,9 +4,22 @@ adjective = input("Enter an adjective: ")
 verb = input("Enter a verb: ")
 
 # Add 10 more inputs, update your story along the way
-name = input("Enter aname: ")
-noun2 = input("Enter another noun: ")
 
 print(f"{noun} was {adjective} while {verb}. They met {name}")
+
+name = input("Enter a name: ")
+noun2 = input("Enter another noun: ")
+adjective2 = input("Enter an adjective: ")
+color = input("Enter a color: ")
+place = input("Enter a place: ")
+comment = input("enter something they will say: ")
+verb2 = input("Enter a verb: ")
+name2 = input("Enter another name: ")
+restaurant  = input("Enter a restaurant: ")
+adjective3 = input("Enter a adjective: ")
+
+
+
+
 
 
